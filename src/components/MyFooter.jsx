8b619ba54926386a766929c47bbad3a4201d3bef
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-06 12:00:58 +0100

    most footer done

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,8 +1,69 @@
 import { Component } from "react"
+import { Container, Row, Col, Nav, Navbar, NavDropdown, Image, Button } from "react-bootstrap"
 
 class MyFooter extends Component {
   render() {
-    return <></>
+    return (
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col xs={12} md={9} lg={6}>
+            <Row className="gap-3">
+              {/* Social icons */}
+              <Col xs={12} className="fs-4 d-flex gap-3">
+                <span>
+                  <i className="bi bi-facebook"></i>
+                </span>
+                <span>
+                  <i className="bi bi-instagram"></i>
+                </span>
+                <span>
+                  <i className="bi bi-twitter"></i>
+                </span>
+                <span>
+                  <i className="bi bi-youtube"></i>
+                </span>
+              </Col>
+
+              {/* Links */}
+              <Col xs={12}>
+                <Row className="g-2 links">
+                  <Col xs={6} md={3}>
+                    <p>Audio and Subtitles</p>
+                    <p>Media Center</p>
+                    <p>Privacy</p>
+                    <p>Contact Us</p>
+                  </Col>
+
+                  <Col xs={6} md={3}>
+                    <p>Audio Description</p>
+                    <p>Investor Relations</p>
+                    <p>Legal Notices</p>
+                  </Col>
+
+                  <Col xs={6} md={3}>
+                    <p>Help Center</p>
+                    <p>Jobs</p>
+                    <p>Cookie Preferences</p>
+                  </Col>
+
+                  <Col xs={6} md={3}>
+                    <p>Gift Cards</p>
+                    <p>Terms of Use</p>
+                    <p>Corporate Information</p>
+                  </Col>
+                </Row>
+              </Col>
+
+              {/* Copyright */}
+              <Col xs={12}>
+                <Button variant="outline-light">Service Code</Button>
+                <p className="mt-2">1997-2019 Netflix, Inc.</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
 
