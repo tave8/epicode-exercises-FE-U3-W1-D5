8fa72f769ea9e4aa10ebb7c6f4b17593373e7b86
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-06 14:44:16 +0100

    fix navbar

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -5,64 +5,69 @@ import { Search, BellFill } from "react-bootstrap-icons"
 class MyNav extends Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          {/* Logo */}
-          <Navbar.Brand href="./">
-            <Image src="/img/netflix_logo.png" style={{ maxWidth: "80px" }} fluid />
-          </Navbar.Brand>
+<Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
 
-          {/* Hamburger */}
-          <Navbar.Toggle aria-controls="main-navbar" />
+        {/* Logo */}
+        <Navbar.Brand href="./">
+          <Image src="/img/netflix_logo.png" fluid style={{ maxWidth: "80px" }} />
+        </Navbar.Brand>
 
-          <Navbar.Collapse id="main-navbar">
-            {/* Left menu */}
-            <Nav className="me-auto">
-              <Nav.Link href="./" className="text-white">
-                Home
-              </Nav.Link>
-              <Nav.Link href="#" className="text-white">
-                TV Shows
-              </Nav.Link>
-              <Nav.Link href="#" className="text-white">
-                Movies
-              </Nav.Link>
-              <Nav.Link href="#" className="text-white">
-                Recently Added
-              </Nav.Link>
-              <Nav.Link href="#" className="text-white">
-                My List
-              </Nav.Link>
-            </Nav>
+        {/* Hamburger */}
+        <Navbar.Toggle aria-controls="navbarSupportedContent" className="btn-light" />
 
-            {/* Right side */}
-            <Nav className="d-flex align-items-center gap-4">
-              <Nav.Link className="text-white">
-                <Search />
-              </Nav.Link>
+        {/* Collapsible content */}
+        <Navbar.Collapse id="navbarSupportedContent">
+          {/* Left menu items */}
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <Nav.Item>
+              <Nav.Link href="./" className="text-white">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" className="text-white">TV Shows</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" className="text-white">Movies</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" className="text-white">Recently Added</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" className="text-white">My List</Nav.Link>
+            </Nav.Item>
+          </Nav>
 
-              <Nav.Link className="text-white">KIDS</Nav.Link>
+          {/* Right options */}
+          <ul className="d-flex justify-content-center align-items-center gap-4 m-0 p-0">
+            <li>
+              <Search className="text-white" />
+            </li>
+            <li className="text-white">KIDS</li>
+            <li>
+              <BellFill className="text-white" />
+            </li>
+            <li>
+              <div className="dropdown-center ms-auto">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant="transparent"
+                    className="bg-transparent border-0 p-0"
+                    id="profile-dropdown"
+                  >
+                    <Image src="https://placedog.net/50/50" fluid style={{ maxWidth: "20px" }} roundedCircle />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu className="dropdown-menu-end">
+                    <Dropdown.Item href="./profile.html">Profile</Dropdown.Item>
+                    <Dropdown.Item href="./settings.html">Settings</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </li>
+          </ul>
+        </Navbar.Collapse>
 
-              <Nav.Link className="text-white">
-                <BellFill />
-              </Nav.Link>
-
-              {/* Profile dropdown */}
-              <Dropdown align="end">
-                <Dropdown.Toggle variant="transparent" className="border-0">
-                  <Image src="https://placedog.net/50/50" roundedCircle style={{ width: "20px" }} />
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  <Dropdown.Item href="./profile.html">Profile</Dropdown.Item>
-                  <Dropdown.Item href="./settings.html">Settings</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      </Container>
+    </Navbar>
     )
   }
 }
